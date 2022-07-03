c3025a02b56af2acc51361ee0af31341c5fe3008
inheritance10 <ali552001a@gmail.com>
2022-07-03 22:54:35 +0300

    slider tamamlandı

--- script.js
+++ script.js
@@ -12,13 +12,55 @@ var models = [
     }
 ];
 
-
-
-
+var interval;
 var index = 0;
 
 var sagIcon = document.querySelector('.fa-arrow-right');
 var solIcon = document.querySelector('.fa-arrow-left');
+
+
+document.querySelectorAll('.arrow').forEach(function (item){
+   item.addEventListener('mouseenter',function (){
+       clearInterval(interval);
+   })
+});
+
+document.querySelectorAll('.arrow').forEach(function (item){
+    item.addEventListener('mouseleave',function (){
+        init(settings);
+    })
+})
+
+var settings = {
+    duration : "1000",
+    random : false
+};
+
+init(settings);
+
+function init(settings){
+    var prev;
+    interval = setInterval(function (){
+
+        if(settings.random){
+            do{
+                index = Math.floor(Math.random() * models.length);
+            }while (index == prev)
+            prev = index;
+            document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+        }else{
+            if(index>2){
+              index = 0;
+            }
+            document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+            index++;
+            console.log(index);
+        }
+
+    },settings.duration);
+}
+
+
 
 sagIcon.addEventListener('click',function (){
     index++;
